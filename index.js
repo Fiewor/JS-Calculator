cf9ccaf26e5199acb47dfb348b433dc64da2b37d
John Fiewor <johnfiewor@gmail.com>
2021-06-08 10:15:20 +0100

    added operators, active highlight, evenlistener

---
+++ index.js
@@ -0,0 +1,11 @@
+for(let butt of document.querySelectorAll(".btn")) {
+    butt.addEventListener("click", () => console.log(this.innerHTML));
+}
+
+// calc = (num1, num2, operator) => {return operator(num1, num2)};
+// add = (num1, num2) => {return num1 + num2};
+
+// constructor
+// function new Operator = {
+//     this.name = name;
+// }
